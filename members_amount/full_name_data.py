13d1a972c43dde1bcb956c5b518232c825893528
Papsik09 <dmtrklymenko05@gmail.com>
2025-01-21 21:02:39 -0800

    First template of Project

---
+++ members_amount/full_name_data.py
@@ -0,0 +1,3 @@
+"""
+Here will be name data
+"""
---
+++ members_amount/main_data.py
@@ -0,0 +1,13 @@
+"""
+General code work will be here
+"""
+print("""Welcome to the data of workers.\n 
+      Please input your costly data to us.
+      """)
+full_name_data = int(input("""Your full name:\n 
+                           Example: Andrew Black"""))
+birthday_data = int(input("""Your data:\n
+                          Example: 04/20/1999"""))
+residence_data = int(input("""Your residence:\n 
+                           Example: Berlin, Germany"""))
+
